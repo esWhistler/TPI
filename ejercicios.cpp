61e8783e406b06c2f0b09edf7b20ef4e17f67646
Your Name <santi.jose.silva@gmail.com>
2021-06-12 20:49:38 -0300

    agregando archivos

---
+++ ejercicios.cpp
@@ -0,0 +1,56 @@
+#include <algorithm>
+#include "ejercicios.h"
+
+// EJERCICIO 1
+bool posicionValida ( pair < tablero, int > const &p ) {
+    bool resp = false;
+    // completar codigo
+    return resp;
+}
+
+// EJERCICIO 2
+bool posicionInicial ( posicion const &p ) {
+    bool resp = false;
+    // completar codigo
+    return resp;
+}
+// EJERCICIO 3
+vector <coordenada> casillasAtacadas ( posicion const &p, int j ) {
+    vector <coordenada> cA;
+    // completar codigo
+    return cA;
+}
+// EJERCICIO 4
+bool posicionSiguienteCorrecta ( posicion const &p1, posicion const &p2, coordenada o, coordenada d ) {
+    bool resp = false;
+    // completar codigo
+    return resp;
+}
+// EJERCICIO 5
+void ordenarTablero ( posicion &p ) {
+    // completar codigo
+    return;
+}
+// EJERCICIO 6
+bool finDeLaPartida ( posicion const &p, int &j ) {
+    bool resp = false;
+    // completar codigo
+    return resp;
+}
+// EJERCICIO 7
+bool hayJaqueDescubierto ( posicion const &p ) {
+    bool resp = false;
+    // completar codigo
+    return resp;
+}
+// EJERCICIO 8
+void ejecutarSecuenciaForzada ( posicion &p, secuencia s ) {
+    // completar codigo
+    return;
+}
+// EJERCICIO 9
+int seVieneElJaqueEn ( posicion const & p ) {
+    int resp = -1;
+    // completar codigo
+    return resp;
+}
