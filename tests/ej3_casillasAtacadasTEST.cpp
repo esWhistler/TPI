53e0a9a12d604cabdd96153e678aa9148baa5770
cpbeto <41555339+cpbeto@users.noreply.github.com>
2021-06-14 19:23:20 -0300

    Agregar tests ej3
    Tests para movimientoValidoAlfil y movimientoValidoTorre.
    Con estos tests se alcanza el 100% de cobertura de líneas.

--- tests/ej3_casillasAtacadasTEST.cpp
+++ tests/ej3_casillasAtacadasTEST.cpp
@@ -31,7 +31,7 @@ TEST(casillasAtacadasTEST, atacadasDelJugadorActual) {
 }
 
 TEST(casillasAtacadasTEST, atacadasDelContrincante) {
-	tablero t = {
+    tablero t = {
             {cVACIA, cREY_N, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA},
             {cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA},
             {cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA},
@@ -41,16 +41,66 @@ TEST(casillasAtacadasTEST, atacadasDelContrincante) {
             {cVACIA, cVACIA, cVACIA, cVACIA, cREY_B, cVACIA, cVACIA, cVACIA},
             {cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA},
     };
-	posicion p = make_pair(t, BLANCO);
+    posicion p = make_pair(t, BLANCO);
 
-	int j = NEGRO;
+    int j = NEGRO;
 
-	vector<coordenada> esperado = {
-		make_pair(0,0), make_pair(0,2), make_pair(1,0), make_pair(1,1), make_pair(1,2), //Rey Negro en (0,1)
-		make_pair(4,5), make_pair(4,7) //Peon Negro en (3,6)
+    vector<coordenada> esperado = {
+            make_pair(0,0), make_pair(0,2), make_pair(1,0), make_pair(1,1), make_pair(1,2), //Rey Negro en (0,1)
+            make_pair(4,5), make_pair(4,7) //Peon Negro en (3,6)
 
-	};
+    };
 
-	vector<coordenada> atacadas = casillasAtacadas(p, j);
-	EXPECT_EQ(ordenarVectorPares(esperado), ordenarVectorPares(atacadas));
+    vector<coordenada> atacadas = casillasAtacadas(p, j);
+    EXPECT_EQ(ordenarVectorPares(esperado), ordenarVectorPares(atacadas));
+}
+
+TEST(casillasAtacadasTEST, atacadasDeAlfil) {
+    tablero t = {
+            {cVACIA, cREY_N, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA},
+            {cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cPEON_N, cVACIA},
+            {cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA},
+            {cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cPEON_N, cVACIA},
+            {cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA},
+            {cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA},
+            {cVACIA, cVACIA, cVACIA, cREY_B, cVACIA, cVACIA, cVACIA, cVACIA},
+            {cVACIA, cVACIA, cALFIL_B, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA},
+    };
+    posicion p = make_pair(t, BLANCO);
+
+    int j = BLANCO;
+
+    vector<coordenada> esperado = {
+            make_pair(5,2), make_pair(5,3), make_pair(5,4), make_pair(6,2), make_pair(6,4), make_pair(7,2), make_pair(7,3), make_pair(7,4), // Rey Blanco en (6,3)
+            make_pair(6,3), make_pair(5,0), make_pair(6, 1) // Alfil blanco en (5,2)
+
+    };
+
+    vector<coordenada> atacadas = casillasAtacadas(p, j);
+    EXPECT_EQ(ordenarVectorPares(esperado), ordenarVectorPares(atacadas));
+}
+
+TEST(casillasAtacadasTEST, atacadasDeTorre) {
+    tablero t = {
+            {cVACIA, cREY_N, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA},
+            {cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cPEON_N, cVACIA},
+            {cVACIA, cVACIA, cVACIA, cVACIA, cREY_B, cVACIA, cTORRE_B, cVACIA},
+            {cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cPEON_N, cVACIA},
+            {cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA},
+            {cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA},
+            {cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA},
+            {cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA},
+    };
+    posicion p = make_pair(t, BLANCO);
+
+    int j = BLANCO;
+
+    vector<coordenada> esperado = {
+            make_pair(1,3), make_pair(1,4), make_pair(1,5), make_pair(2,3), make_pair(2,5), make_pair(3,3), make_pair(3,4), make_pair(3,5), // Rey Blanco en (2,4)
+            make_pair(1,6), make_pair(3,6), make_pair(2, 4), /*make_pair(2, 5),*/ make_pair(2, 7) // Torre blanca en (2,6)
+
+    };
+
+    vector<coordenada> atacadas = casillasAtacadas(p, j);
+    EXPECT_EQ(ordenarVectorPares(esperado), ordenarVectorPares(atacadas));
 }
