8dd6567f93b14336d77ebc3f5905544416b9f080
cpbeto <41555339+cpbeto@users.noreply.github.com>
2021-06-14 20:01:22 -0300

    todo: Revisar ej3

--- ejercicios.cpp
+++ ejercicios.cpp
@@ -13,7 +13,7 @@ bool posicionInicial ( posicion const &p ) {
     return posInicial == p;
 }
 // EJERCICIO 3
-// a revisar si hay tiempo
+// todo: a revisar si hay tiempo
 vector <coordenada> casillasAtacadas ( posicion const &p, int j ) {
     tablero tablero = p.first;
     vector<coordenada> cA;
